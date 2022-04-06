9e0e513d3e8133044a9d4576e65e43340689bd08
Dannel Albert <cuebix@gmail.com>
2022-04-05 23:20:19 -0400

    fix: missing text in headers

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,19 @@
 import styles from './sass/Header.module.scss'
 
 export const Header = () => {
-  return (
-  <div className={styles.root}>
-    <div className={styles.wrapper}>
-      <img src='https://picsum.photos/id/1060/2000/600' />
-    </div>
-  </div>
-  )
+    return (
+        <div className={styles.root}>
+            <div
+                className={styles.wrapper}
+                style={{
+                    backgroundImage:
+                        'url(https://picsum.photos/id/1060/2000/600)',
+                }}
+            >
+                <h1>Header Header Header</h1>
+            </div>
+        </div>
+    )
 }
 
 export default Header
--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,13 +1,19 @@
 import styles from './sass/Hero.module.scss'
 
 export const Hero = () => {
-  return (
-  <div className={styles.root}>
-    <div className={styles.wrapper}>
-      <img src='https://picsum.photos/id/42/1024/200' />
-    </div>
-  </div>
-  )
+    return (
+        <div className={styles.root}>
+            <div
+                className={styles.wrapper}
+                style={{
+                    backgroundImage:
+                        'url(https://picsum.photos/id/42/1024/200)',
+                }}
+            >
+                <h2>Hero Text Here</h2>
+            </div>
+        </div>
+    )
 }
 
 export default Hero
