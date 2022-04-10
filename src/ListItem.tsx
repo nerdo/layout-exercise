ebc200439edbace0cc51b0b73724d1f1d85aa924
Dannel Albert <cuebix@gmail.com>
2022-04-10 15:22:52 -0500

    style: prettier

--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -2,26 +2,28 @@ import { ReactNode } from 'react'
 import styles from './sass/ListItem.module.scss'
 
 export interface ListItemProps {
-  imagePath?: string
-  children: ReactNode | ReactNode[]
+    imagePath?: string
+    children: ReactNode | ReactNode[]
 }
 
 export const ListItem = (props: ListItemProps) => {
-  const { children: content, imagePath } = props
-  return (
-    <div className={styles.root}>
-      <div className={styles.wrapper}>
-        <div className={styles['gutter-and-image']}>
-          <div className={styles.gutter}></div>
-          <div className={styles.image}>{imagePath && <img src={imagePath} />}</div>
+    const { children: content, imagePath } = props
+    return (
+        <div className={styles.root}>
+            <div className={styles.wrapper}>
+                <div className={styles['gutter-and-image']}>
+                    <div className={styles.gutter}></div>
+                    <div className={styles.image}>
+                        {imagePath && <img src={imagePath} />}
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    <div className={styles.wrapper}>{content}</div>
+                </div>
+                <div className={styles.gutter}></div>
+            </div>
         </div>
-        <div className={styles.content}>
-          <div className={styles.wrapper}>{content}</div>
-        </div>
-        <div className={styles.gutter}></div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default ListItem
